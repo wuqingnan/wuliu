3a9ab4bce4b8db53069ef6979b27787f150e451d
wuqingnan <46428886@qq.com>
2014-06-17 18:22:57 +0800

    Add MainFragment

--- WLClient/src/com/wuliu/client/activity/MainActivity.java
+++ WLClient/src/com/wuliu/client/activity/MainActivity.java
@@ -3,51 +3,65 @@ package com.wuliu.client.activity;
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 import com.wuliu.client.R;
-import com.wuliu.client.fragment.ColorFragment;
-import com.wuliu.client.fragment.ColorMenuFragment;
+import com.wuliu.client.fragment.MainFragment;
 import com.wuliu.client.fragment.MapFragment;
 import com.wuliu.client.fragment.MenuFragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
 
 public class MainActivity extends SlidingFragmentActivity {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
-	
+
 	private Fragment mContent;
+	private FragmentManager mFragmentManager;
+	
+	private SlidingMenu mSlidingMenu;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_main);
-		
+
+		mFragmentManager = getSupportFragmentManager();
+
 		mContent = new MapFragment();
-		getSupportFragmentManager().beginTransaction()
-		.replace(R.id.mapLayout, mContent).commit();
-		
+
+		mFragmentManager.beginTransaction().replace(R.id.mapLayout, mContent)
+				.commit();
+
+		mFragmentManager.beginTransaction()
+				.replace(R.id.topLayout, new MainFragment()).commit();
+
 		setBehindContentView(R.layout.menu_frame);
-		getSupportFragmentManager().beginTransaction()
+		mFragmentManager.beginTransaction()
 				.replace(R.id.menu_frame, new MenuFragment()).commit();
 
 		initSlidingMenu();
 	}
-	
+
 	private void initSlidingMenu() {
-		SlidingMenu sm = getSlidingMenu();
-		sm.setShadowWidthRes(R.dimen.shadow_width);
+		mSlidingMenu = getSlidingMenu();
+		mSlidingMenu.setShadowWidthRes(R.dimen.shadow_width);
 		// sm.setShadowDrawable(R.drawable.shadow);
-		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-		sm.setFadeDegree(0.35f);
-		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		mSlidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		mSlidingMenu.setFadeDegree(0.35f);
+		mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
 	}
 
+	public void onTitleClick(View view) {
+		mSlidingMenu.showMenu();
+	}
+	
 	public void switchContent(Fragment fragment) {
 		mContent = fragment;
-		getSupportFragmentManager().beginTransaction()
-				.replace(R.id.content_frame, fragment).commit();
-		getSlidingMenu().showContent();
+		// getSupportFragmentManager().beginTransaction()
+		// .replace(R.id.content_frame, fragment).commit();
+		// getSlidingMenu().showContent();
 	}
 
 }
--- WLClient/src/com/wuliu/client/fragment/MainFragment.java
+++ WLClient/src/com/wuliu/client/fragment/MainFragment.java
@@ -1,17 +1,57 @@
 package com.wuliu.client.fragment;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+import com.wuliu.client.R;
+import com.wuliu.client.activity.MainActivity;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainFragment extends Fragment {
 
+	private static final String TAG = MainFragment.class.getSimpleName();
+	
+	private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View view) {
+			if (getActivity() instanceof MainActivity) {
+				((MainActivity)getActivity()).onTitleClick(view);
+			}
+		}
+	};
+	
+	private View mRootView;
+	
+	@InjectView(R.id.titlebar_leftBtn)
+	ImageView mMenuBtn;
+	@InjectView(R.id.titlebar_title)
+	TextView mTitle;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return super.onCreateView(inflater, container, savedInstanceState);
+		mRootView = inflater.inflate(R.layout.fragment_main, null);
+		return mRootView;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		init();
+	}
+	
+	private void init() {
+		ButterKnife.inject(this, mRootView);
+		mTitle.setText(R.string.app_name);
+		mMenuBtn.setImageResource(R.drawable.btn_title_menu);
+		mMenuBtn.setOnClickListener(mOnClickListener);
 	}
 	
 }
