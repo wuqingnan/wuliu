f96b4b7bb5119d05dd4f954a248bdc4ffc5a45fa
wuqingnan <46428886@qq.com>
2014-12-12 21:38:37 +0800

    Modify OrderListFragment

--- Driver/src/cn/boweikeji/wuliu/driver/fragment/OrderListFragment.java
+++ Driver/src/cn/boweikeji/wuliu/driver/fragment/OrderListFragment.java
@@ -9,7 +9,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.baidu.location.BDLocation;
-
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import butterknife.ButterKnife;
@@ -18,8 +17,6 @@ import cn.boweikeji.wuliu.driver.Const;
 import cn.boweikeji.wuliu.driver.R;
 import cn.boweikeji.wuliu.driver.WLApplication;
 import cn.boweikeji.wuliu.driver.activity.OrderDetailActivity;
-import cn.boweikeji.wuliu.driver.activity.FindResultActivity.FindAdapter;
-import cn.boweikeji.wuliu.driver.activity.FindResultActivity.ViewHolder;
 import cn.boweikeji.wuliu.driver.api.BaseParams;
 import cn.boweikeji.wuliu.driver.bean.Order;
 import cn.boweikeji.wuliu.driver.bean.UserInfo;
@@ -56,27 +53,26 @@ public class OrderListFragment extends BaseFragment {
 	private SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
 		@Override
 		public void onRefresh() {
-			 refresh();
+			refresh();
 		}
 	};
 
 	private JsonHttpResponseHandler mRequestHandler = new JsonHttpResponseHandler() {
 
 		public void onFinish() {
-			 loadFinish();
+			loadFinish();
 		};
 
 		public void onSuccess(int statusCode, Header[] headers,
 				JSONObject response) {
-			 requestResult(response);
+			requestResult(response);
 		};
 
 		public void onFailure(int statusCode, Header[] headers,
 				Throwable throwable, JSONObject errorResponse) {
-			 requestResult(null);
+			requestResult(null);
 		};
 	};
-
 
 	private AbsListView.OnScrollListener mOnScrollListener = new AbsListView.OnScrollListener() {
 		@Override
@@ -85,24 +81,26 @@ public class OrderListFragment extends BaseFragment {
 				loadMore();
 			}
 		}
-		
+
 		@Override
-		public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-			
+		public void onScroll(AbsListView view, int firstVisibleItem,
+				int visibleItemCount, int totalItemCount) {
+
 		}
 	};
-	
+
 	private AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
 				long id) {
 			if (id >= 0) {
-				Order order = (Order) mAdapter.getItem((int)id);
-				OrderDetailActivity.startOrderDetailActivity(getActivity(), order.getGoods_cd());
+				Order order = (Order) mAdapter.getItem((int) id);
+				OrderDetailActivity.startOrderDetailActivity(getActivity(),
+						order.getGoods_cd());
 			}
 		}
 	};
-	
+
 	private View mRootView;
 
 	@InjectView(R.id.swiperefresh)
@@ -137,7 +135,7 @@ public class OrderListFragment extends BaseFragment {
 		mRootView = inflater.inflate(R.layout.fragment_order_list, null);
 		return mRootView;
 	}
-	
+
 	@Override
 	public void onDestroyView() {
 		super.onDestroyView();
@@ -159,10 +157,11 @@ public class OrderListFragment extends BaseFragment {
 	}
 
 	private void initView() {
-		mFooter = getActivity().getLayoutInflater().inflate(R.layout.load_layout, null);
+		mFooter = getActivity().getLayoutInflater().inflate(
+				R.layout.load_layout, null);
 		mSwipeRefreshLayout.setOnRefreshListener(mOnRefreshListener);
-		mSwipeRefreshLayout.setColorSchemeResources(R.color.color1, R.color.color2, R.color.color3,
-                R.color.color4);
+		mSwipeRefreshLayout.setColorSchemeResources(R.color.color1,
+				R.color.color2, R.color.color3, R.color.color4);
 		mListView.addFooterView(mFooter);
 		mAdapter = new OrderListAdapter(getActivity(), mType);
 		mListView.setAdapter(mAdapter);
@@ -170,26 +169,29 @@ public class OrderListFragment extends BaseFragment {
 		mListView.setOnScrollListener(mOnScrollListener);
 		mListView.setOnItemClickListener(mOnItemClickListener);
 	}
-	
+
 	private void initData() {
+		mMore = true;
+		mLoading = false;
 		refresh();
 	}
 
 	private void refresh() {
-    	if (mLoading) {
-    		return;
-    	}
-    	mMore = true;
-    	mPage = 1;
-    	mEmptyView.setVisibility(View.GONE);
-    	mSwipeRefreshLayout.setEnabled(false);
-    	mSwipeRefreshLayout.setRefreshing(true);
-    	loadData();
-    }
-	
+		if (mLoading) {
+			return;
+		}
+		mMore = true;
+		mPage = 1;
+		mEmptyView.setVisibility(View.GONE);
+		mSwipeRefreshLayout.setEnabled(false);
+		mSwipeRefreshLayout.setRefreshing(true);
+		loadData();
+	}
+
 	private void loadData() {
 		mLoading = true;
-		BDLocation location = WLApplication.getLocationClient().getLastKnownLocation();
+		BDLocation location = WLApplication.getLocationClient()
+				.getLastKnownLocation();
 		BaseParams params = new BaseParams();
 		params.add("method", "getMyRecords");
 		params.add("page_num", "" + mPage);
@@ -223,30 +225,30 @@ public class OrderListFragment extends BaseFragment {
 			params.add("passwd", BaseParams.PARAM_DEFAULT);
 		}
 		AsyncHttp.get(Const.URL_ORDER_LIST, params, mRequestHandler);
-    }
-	  
-    private void loadMore() {
-    	if (!mMore) {
-    		return;
-    	}
-    	if (mLoading) {
-    		return;
-    	}
-    	loadData();
-    }
-	
-    private void requestResult(JSONObject response) {
-    	mLoading = false;
-    	if (!isAdded()) {
-    		return;
-    	}
+	}
+
+	private void loadMore() {
+		if (!mMore) {
+			return;
+		}
+		if (mLoading) {
+			return;
+		}
+		loadData();
+	}
+
+	private void requestResult(JSONObject response) {
+		mLoading = false;
+		if (!isAdded()) {
+			return;
+		}
 		if (response != null && response.length() > 0) {
 			Log.d(TAG, "shizy---response: " + response.toString());
 			try {
 				int res = response.getInt("res");
 				String msg = response.getString("msg");
 				Util.showTips(getActivity(), msg);
-				if (res == 2) {//成功
+				if (res == 2) {// 成功
 					int pageCount = response.getInt("pagetotalnum");
 					if (pageCount <= 0) {
 						mEmptyView.setVisibility(View.VISIBLE);
@@ -266,12 +268,16 @@ public class OrderListFragment extends BaseFragment {
 							order.setGoods_cd(temp.optString("goods_cd"));
 							order.setGoods_name(temp.optString("goods_name"));
 							if (mType == TYPE_SELECT) {
-								order.setIs_order(Integer.parseInt(temp.optString("is_order")));
-								order.setDistance(Double.parseDouble(temp.optString("distance")));
+								order.setIs_order(Integer.parseInt(temp
+										.optString("is_order")));
+								order.setDistance(Double.parseDouble(temp
+										.optString("distance")));
 							} else if (mType == TYPE_COMPLETED) {
-								order.setIs_ticked(Integer.parseInt(temp.optString("is_ticked")));
+								order.setIs_ticked(Integer.parseInt(temp
+										.optString("is_ticked")));
 							} else if (mType == TYPE_CANCEL) {
-								order.setState(Integer.parseInt(temp.optString("state")));
+								order.setState(Integer.parseInt(temp
+										.optString("state")));
 							}
 							data.add(order);
 						}
@@ -302,27 +308,33 @@ public class OrderListFragment extends BaseFragment {
 		}
 		Util.showTips(getActivity(), getString(R.string.request_failed));
 	}
-    
-    private void loadFinish() {
-    	mSwipeRefreshLayout.setEnabled(true);
-    	mSwipeRefreshLayout.setRefreshing(false);
-    }
-    
-    public void onEventMainThread(OrderEvent event) {
-    	refresh();
-    }
-	
+
+	private void loadFinish() {
+		mSwipeRefreshLayout.setEnabled(true);
+		mSwipeRefreshLayout.setRefreshing(false);
+	}
+
+	public void onEventMainThread(OrderEvent event) {
+		refresh();
+	}
+
 	public static class OrderListAdapter extends BaseAdapter {
 
 		private LayoutInflater mInflater;
 		private Context mContext;
 		private List<Order> mData;
 		private int mType;
+		private String[] mStateName;
+		private int[] mStateValue;
 
 		public OrderListAdapter(Context context, int type) {
 			mType = type;
 			mContext = context;
 			mInflater = LayoutInflater.from(context);
+			mStateName = context.getResources().getStringArray(
+					R.array.order_state_name);
+			mStateValue = context.getResources().getIntArray(
+					R.array.order_state_value);
 		}
 
 		public void setData(List<Order> data) {
@@ -362,16 +374,31 @@ public class OrderListFragment extends BaseFragment {
 		public View getView(int position, View convertView, ViewGroup viewGroup) {
 			ViewHolder holder = null;
 			if (convertView == null) {
-				convertView = mInflater
-						.inflate(R.layout.order_list_item, null);
+				convertView = mInflater.inflate(R.layout.order_list_item, null);
 				holder = new ViewHolder(convertView);
 				convertView.setTag(holder);
 			} else {
 				holder = (ViewHolder) convertView.getTag();
 			}
 			Order order = mData.get(position);
-			holder.refresh(order, mType);
+			if (mType == TYPE_CANCEL) {
+				holder.refresh(order, mType,
+						mStateName[getStateIndexByValue(order.getState())]);
+			} else {
+				holder.refresh(order, mType);
+			}
 			return convertView;
+		}
+
+		private int getStateIndexByValue(int value) {
+			int index = 0;
+			for (int i = 0; i < mStateValue.length; i++) {
+				if (mStateValue[i] == value) {
+					index = i;
+					break;
+				}
+			}
+			return index;
 		}
 
 	}
@@ -394,17 +421,24 @@ public class OrderListFragment extends BaseFragment {
 		}
 
 		public void refresh(Order order, int type) {
+			refresh(order, type, null);
+		}
+
+		public void refresh(Order order, int type, String stateName) {
 			if (order != null) {
 				mOrderName.setText(order.getGoods_name());
-				mOrderCode.setText(String.format("订单号：%s", order.getGoods_cd()));
+				mOrderCode
+						.setText(String.format("订单号：%s", order.getGoods_cd()));
 				mOrderDate.setText(order.getCreate_date());
 			}
 			switch (type) {
 			case TYPE_SELECT:
 				mBespeak.setVisibility(View.VISIBLE);
 				mOrderInfo.setVisibility(View.VISIBLE);
-				mBespeak.setImageResource(order.isOrder() ? R.drawable.ic_bespeak : R.drawable.ic_actual);
-				mOrderInfo.setText(String.format("距离%d米", (int)order.getDistance()));
+				mBespeak.setImageResource(order.isOrder() ? R.drawable.ic_bespeak
+						: R.drawable.ic_actual);
+				mOrderInfo.setText(String.format("距离%d米",
+						(int) order.getDistance()));
 				break;
 			case TYPE_SELECTED:
 				mBespeak.setVisibility(View.GONE);
@@ -418,7 +452,7 @@ public class OrderListFragment extends BaseFragment {
 			case TYPE_CANCEL:
 				mBespeak.setVisibility(View.GONE);
 				mOrderInfo.setVisibility(View.VISIBLE);
-				mOrderInfo.setText("" + order.getState());
+				mOrderInfo.setText(stateName);
 				break;
 			}
 		}
