49a6e8ad94114641abc80944e5c79227ef9c4ce6
wuqingnan <46428886@qq.com>
2014-12-22 15:30:07 +0800

    Update UMeng

--- Supplyer/src/cn/boweikeji/wuliu/supplyer/activity/MainActivity.java
+++ Supplyer/src/cn/boweikeji/wuliu/supplyer/activity/MainActivity.java
@@ -271,7 +271,6 @@ public class MainActivity extends BaseActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		MobclickAgent.openActivityDurationTrack(false);
 		init();
 	}
 
